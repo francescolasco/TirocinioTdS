9df44980c6df1319551ca53933be386d1b24a83d
francesco <francescolasco@gmail.com>
2021-03-19 18:22:59 +0100

    primo commit primo compito

---
+++ tirocinio_1/energiaTD.m
@@ -0,0 +1,5 @@
+function J = energiaTD(u)
+%ENERGIATD calcola l'energia del segnale di controllo a Tempo Discreto u
+    J = 0;
+end
+
---
+++ tirocinio_1/myP.m
@@ -0,0 +1,5 @@
+function P = myP(A,B,ni)
+%MYP Restituisce la matrice di raggiungibilità in ni passi.
+
+end
+
